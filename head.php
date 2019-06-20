f8e95b57b226b5bbb7dffcbc59da195e7836fd5e
MagicMarvMan <marvin.kai@gmx.de>
2019-06-20 17:45:40 +0200

    Add files

---
+++ head.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title><?php if(isset($meta_title)) { echo($meta_title . " > "); } ?>Briefe</title>
+
+    <link rel="stylesheet" type="text/css" href="https://cdn.marvnet.digital/webassets/bootstrap/bootswatch/darkly/bootstrap.min.css">
+</head>
