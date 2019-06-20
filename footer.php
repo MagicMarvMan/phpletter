e014391820a6555f16869775f676ca3e037ba12c
MagicMarvMan <marvin.kai@gmx.de>
2019-06-20 17:51:10 +0200

    Add javascript files

--- footer.php
+++ footer.php
@@ -1,4 +1,7 @@
 
 
+    <script type="application/javascript" src="https://cdn.marvnet.digital/webassets/popper/1.0.0/popper.min.js"></script>
+    <script type="application/javascript" src="https://cdn.marvnet.digital/webassets/jquery/3.4.1/jquery.min.js"></script>
+    <script type="application/javascript" src="https://cdn.marvnet.digital/webassets/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
